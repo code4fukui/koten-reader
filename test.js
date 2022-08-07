44c1797dc9d64df5cef905e8aec6ed971af2a0db
Taisuke Fukuno <fukuno@jig.jp>
2022-08-07 11:25:43 +0900

    add parameter

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { parseText } from "./parseText.js";
 import { SJIS } from "https://js.sabae.cc/SJIS.js";
 
 //const fn = "3804_27277.html";
-const url = "https://www.aozora.gr.jp/cards/001395/files/49897_42649.html"; // 創作
+const url = Deno.args[0] || "https://www.aozora.gr.jp/cards/001395/files/49897_42649.html"; // 創作
 //const fn = "92_14545.html";
 //const bin = await Deno.readFile("data/" + fn);
 const bin = new Uint8Array(await (await fetch(url)).arrayBuffer());
